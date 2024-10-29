d7e3143af1a5794beff5e5c209a0d777d3342780
Paul Winkler <slinkp@gmail.com>
2024-10-29 17:27:53 -0400

    First try at prompts for company research via tavily

--- tavily_research_demo.py
+++ tavily_research_demo.py
@@ -29,13 +29,65 @@ class ResearchAgent:
         return result["output"]
 
 
+HEADCOUNT_PROMPT = """
+For the company {company_name}, find:
+   - the number of employees
+   - the number of employees at the NYC office, if there is one.
+   - the number of employees who are egineers, if known.
+   - the URLs of the pages that contain the information above
+Return these results as a valid JSON object, with the following keys:
+    - headcount
+    - headcount_nyc
+    - headcount_engineers
+    - citation_urls
+
+Value of citation_urls should be a list of strings.
+Values of other keys should be integers, if known. If value is not known, it should be null.
+Return ONLY the JSON object, nothing else.
+"""
+
+OFFICE_PROMPT = """
+For the company {company_name}, find:
+    - the city of the company's headquarters
+    - the company's remote work policy
+    - the URLs of the pages that contain the information above
+
+Return these results as a valid JSON object, with the following keys:
+    - headquarters_city
+    - remote_work_policy
+    - citation_urls
+
+Values should be strings, if known. If value is not known, it should be null.
+The remote work policy should be one of the following:
+    - "hybrid"
+    - "remote"
+    - "in-person"
+    - null
+
+Return ONLYthe JSON object, nothing else.
+"""
+
+PROMPTS = [
+    OFFICE_PROMPT,
+    HEADCOUNT_PROMPT,
+]
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("query", type=str)
+    parser.add_argument("--query", type=str, default="")
+    parser.add_argument("--company", type=str, default="")
     parser.add_argument("--verbose", action="store_true")
     args = parser.parse_args()
 
     agent = ResearchAgent(verbose=args.verbose)
-    print(agent.single_search(args.query))
+    if args.query:
+        print(agent.single_search(args.query))
+    elif args.company:
+        for prompt in PROMPTS:
+            prompt = prompt.format(company_name=args.company)
+            print(agent.single_search(prompt))
+    else:
+        print("No query or company provided")
