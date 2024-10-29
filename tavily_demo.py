daef588cf6ffc4bd5f35754e20287cd072372fea
Paul Winkler <slinkp@gmail.com>
2024-10-29 11:57:40 -0400

    Fix some deprecation warnings in tavily_demo. There's still one more to fix

--- tavily_demo.py
+++ tavily_demo.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain.agents import initialize_agent, AgentType
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search.tool import TavilySearchResults
 
 # set up API key via direnv
@@ -29,6 +29,6 @@ You must always output a valid JSON object with the following keys:
 The values must be integers, or null if unknown.
 """
 # run the agent
-agent_chain.run(
+agent_chain.invoke(
     PROMPT,
 )
