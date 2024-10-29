4da9e0fa6b4cfc9c587eb26ecc65ebad6cd7af68
Paul Winkler <slinkp@gmail.com>
2024-10-29 15:46:19 -0400

    Tavily demo: Found the new API for getting an AgentExecutor which is what the old thing actually did

--- tavily_demo.py
+++ tavily_demo.py
@@ -1,26 +1,19 @@
 import os
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search.tool import TavilySearchResults
 
-# set up API key via direnv
+# I set up API key via direnv
 
 # set up the agent
 llm = ChatOpenAI(model_name="gpt-4", temperature=0.7)
 search = TavilySearchAPIWrapper()
 tavily_tool = TavilySearchResults(api_wrapper=search)
 
-# initialize the agent
-agent_chain = initialize_agent(
-    [tavily_tool],
-    llm,
-    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-)
+agent_chain = create_conversational_retrieval_agent(llm, [tavily_tool], verbose=True)
 
-
-PROMPT = """
+QUESTION = """
 What are the headcounts of Rokt at https://rokt.com?
 Look for information about the number of employees,
 the number of engineers, and the number of employees in NYC.
@@ -30,5 +23,5 @@ The values must be integers, or null if unknown.
 """
 # run the agent
 agent_chain.invoke(
-    PROMPT,
+    QUESTION,
 )
