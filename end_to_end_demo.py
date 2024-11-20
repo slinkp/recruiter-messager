a89aba28a5f49dd265d6ee2af20dfe54ce4ad81d
Paul Winkler <slinkp@gmail.com>
2024-11-20 16:20:44 -0500

    WIP: working proof-of-concept of fetching recruiter pings and generating replies to them

--- end_to_end_demo.py
+++ end_to_end_demo.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os.path
 import company_researcher
 from rag import RecruitmentRAG
@@ -30,6 +31,7 @@ def followup_research_company(company_info: company_researcher.CompanyInfo):
     # TODO: Implement this:
     # - use linkedin_searcher.py to find contacts
     # - Store those somewhere.  Where? Spreadsheet - update existing row?
+    logger.info(f"Doing followup research on: {company_info}")
     return company_info
 
 
@@ -76,7 +78,7 @@ class EmailResponder:
         rag = RecruitmentRAG(old_messages, loglevel=self.loglevel)
         rag.prepare_data(clear_existing=not self.use_cache)
         rag.setup_chain(llm_type=self.reply_rag_model)
-        print(f"RAG setup complete")
+        logger.info(f"RAG setup complete")
         return rag
 
     def load_previous_replies_to_recruiters(self) -> list[tuple[str, str, str]]:
@@ -86,15 +88,15 @@ class EmailResponder:
             try:
                 with open(cachefile, "r") as f:
                     old_replies = json.load(f)
-                    print(f"Loaded {len(old_replies)} messages from cache")
+                    logger.info(f"Loaded {len(old_replies)} old replies from cache")
             except FileNotFoundError:
-                print("No cache found, rebuilding...")
+                logger.warning("No cache found, rebuilding...")
         if not old_replies:
-            print("Fetching messages from mail...")
+            logger.info("Fetching my previous replies from mail...")
             old_replies = self.email_client.get_my_replies_to_recruiters(
                 max_results=self.reply_rag_limit
             )
-            print(f"Got messages from mail: {len(old_replies)}")
+            logger.info(f"Got my replies from mail: {len(old_replies)}")
             with open(cachefile, "w") as f:
                 json.dump(old_replies, f, indent=2)
 
@@ -103,8 +105,48 @@ class EmailResponder:
     def generate_reply(self, msg: str) -> str:
         return self.rag.generate_reply(msg)
 
-    def get_new_recruiter_messages(self) -> list[tuple[str, str, str]]:
-        return self.email_client.get_new_recruiter_messages()
+    def get_new_recruiter_messages(
+        self, max_results: int = 500
+    ) -> list[tuple[str, str, str]]:
+        messsage_dicts = self.email_client.get_new_recruiter_messages(
+            max_results=max_results
+        )
+        # TODO: Move this to email_client.py
+        # TODO: optionally cache it
+        # TODO: solve for linkedin's failure to thread emails from DMs
+        # TODO: solve for linkedin's stupidly threading "join your network" emails from different people
+
+        # Combine messages in each thread
+        content_by_thread = defaultdict(list)
+        for msg in messsage_dicts:
+            thread_id = msg["threadId"]
+            content = self.email_client.extract_message_content(msg)
+            content = self.email_client.clean_quoted_text(content)
+            date = msg["internalDate"]
+            content_by_thread[thread_id].append((date, content, msg))
+
+        combined_messages = []
+        for thread_id, msg_list in content_by_thread.items():
+            # Sort a thread by date, oldest first.
+            msg_list.sort(key=lambda x: x[0])
+            combined_msg = msg_list[-1][-1].copy()  # Use the latest dict
+            # Concatenate the text content of all messages in the thread
+            combined_content = [mdict[1] for mdict in msg_list]
+            if len(combined_content) > 1:
+                for i, content in enumerate(combined_content):
+                    logger.debug(f"Thread {thread_id} content {i}:\n{content[:200]}...")
+            else:
+                # HACK: For test purposes i'm only interested in combined messages
+                continue
+
+            combined_msg["combined_content"] = "\n\n".join(combined_content)
+            combined_messages.append(combined_msg)
+
+        logger.info(
+            f"Got {len(messsage_dicts)} new recruiter messages in {len(combined_messages)} threads"
+        )
+        combined_messages.sort(key=lambda x: int(x["internalDate"]), reverse=True)
+        return combined_messages
 
 
 def main(args, loglevel: int = logging.INFO):
@@ -118,16 +160,23 @@ def main(args, loglevel: int = logging.INFO):
     # The email_client code doesn't have this yet.
     # It should combine multiple emails from the same recruiter.
 
-    if len(args.test_messages):
+    if args.test_messages:
+        # TODO: These are just raw strings. Fake some dicts for them.
         new_recruiter_email = args.test_messages
     else:
         new_recruiter_email = email_responder.get_new_recruiter_messages()
 
     for msg in new_recruiter_email:
-        print(f"Processing message: {msg}")
-        company_info = initial_research_company(msg)
-        reply = email_responder.generate_reply(msg)
-        logger.info(reply)
+        # Log the subject
+        content = msg.get("combined_content")
+        logger.info(
+            f"==============================\n\nProcessing message:\n\n{content}\n"
+        )
+        # TODO: pass subject too?
+        company_info = initial_research_company(content)
+        logger.info(f"Company info: {company_info}\n\n")
+        reply = email_responder.generate_reply(content)
+        logger.info(f"------ GENERATED REPLY:\n{reply}\n\n")
         if is_good_fit(company_info):
             company_info = followup_research_company(company_info)
 
